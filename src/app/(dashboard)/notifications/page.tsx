c3be8b8dcb5db267cd372e9797cc848a4db9ca10
nrbnayon <nrbnayon@gmail.com>
2025-08-02 17:50:29 +0600

    Complete

--- src/app/(dashboard)/notifications/page.tsx
+++ src/app/(dashboard)/notifications/page.tsx
@@ -5,8 +5,10 @@ export default function NotificationsPage() {
   return (
     <div>
       <DashboardHeader title="Welcome  Nayon" />
-      <div className="p-2 md:px-6 py-2 border border-primary/30 rounded-2xl">
-        <Notifications />
+      <div className="p-2 md:px-6 ">
+        <div className="py-2 border border-primary/30 rounded-2xl">
+          <Notifications />
+        </div>
       </div>
     </div>
   );
